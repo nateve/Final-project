adec1c5ac720615f3bbb6285b25c97d05e60eabe
Natalie DeClerck <ndecle2@illinois.edu>
2017-12-12 23:37:21 -0600

    computing sample statistics for n observations of each parameter

--- stoplight.py
+++ stoplight.py
@@ -1,6 +1,8 @@
 import simpy
 import random
+import time
 from queue import Queue
+import numpy as np
 
 
 def arrive(queue, direction, light):
@@ -19,7 +21,7 @@ def arrive(queue, direction, light):
                                 "at time {time: .3f}.".format(arrive_count=arrive_count, direction=direction, time=env.now))
 
         if direction == 'N' or direction == 'S':
-            next_arrival = random.uniform(1, 10)
+            next_arrival = random.uniform(1, 30)
         else:
             next_arrival = random.uniform(5, 60)
         yield env.timeout(next_arrival)
@@ -104,45 +106,78 @@ def run_sim(NS_green_time, total_time):
 
     env.run(until=total_time)
 
-    print('\n\n*** Summary ***\n\n')
-    print('Green light time:', NS_green_time)
-    print('Mean waiting time: %.3f seconds'
-      % (Stats['waiting_time'] / float(Stats['car_count'])))
-
-    print('Max waiting time: %.3f seconds'
-      % Stats['max_wait_time'])
-
     if print_sim:
+        print('\n\n*** Summary ***\n\n')
+        print('Green light time:', NS_green_time)
+        print('Mean waiting time: %.3f seconds'
+          % (Stats['waiting_time'] / float(Stats['car_count'])))
+
+        print('Max waiting time: %.3f seconds'
+          % Stats['max_wait_time'])
+
         print('\n')
         for k,v in Stats.items():
             print(k,':',v)
 
 
 if __name__ == '__main__':
+    start = time.time()
+    green_times = []
+    sample_means = []
+    sample_maxs = []
+    std_error_means = []
+    std_error_maxs = []
     print_sim = False
+    n = 200
+    print('N =', n)
+    for green_time in range(1,59,2):
+        means = []
+        maxs = []
+        for i in range(n):
+            env = simpy.Environment()
 
-    for seconds in range(5,55,5):
-        env = simpy.Environment()
+            NS_light = 'green'
+            EW_light = 'red'
+            cur_max_wait = 0
+            arrive_count = 0
 
-        NS_light = 'green'
-        EW_light = 'red'
-        cur_max_wait = 0
-        arrive_count = 0
+            N_queue = Queue()
+            S_queue = Queue()
+            E_queue = Queue()
+            W_queue = Queue()
 
-        N_queue = Queue()
-        S_queue = Queue()
-        E_queue = Queue()
-        W_queue = Queue()
+            Stats = dict()
+            Stats['cars_waiting'] = 0
+            Stats['car_count'] = 0
+            Stats['waiting_time'] = 0.0
+            Stats['max_wait_time'] = 0
+            Stats['red_time'] = 0.0
+            Stats['red_count'] = 0
+            Stats['green_time'] = 0.0
+            Stats['green_count'] = 0
 
-        Stats = dict()
-        Stats['cars_waiting'] = 0
-        Stats['car_count'] = 0
-        Stats['waiting_time'] = 0.0
-        Stats['max_wait_time'] = 0
-        Stats['red_time'] = 0.0
-        Stats['red_count'] = 0
-        Stats['green_time'] = 0.0
-        Stats['green_count'] = 0
+            # run each process for 10 minutes
+            run_sim(NS_green_time=green_time, total_time=600)
 
-        run_sim(NS_green_time=seconds, total_time=3600)
+            means.append(Stats['waiting_time'] / float(Stats['car_count']))
+            maxs.append(Stats['max_wait_time'])
+
+        sample_mean = np.mean(means)
+        std_error_mean = np.std(means) / np.sqrt(n)
+        sample_max = np.mean(maxs)
+        std_error_max = np.std(maxs) / np.sqrt(n)
+
+        sample_means.append(sample_mean)
+        std_error_means.append(std_error_mean)
+        sample_maxs.append(sample_max)
+        std_error_maxs.append(std_error_max)
+        green_times.append(green_time)
+
+    print(green_times)
+    print(sample_means)
+    print(std_error_means)
+    print(sample_maxs)
+    print(std_error_maxs)
+
+    print('Process took', time.time() - start, 'seconds')
 
